8508701b10a08134901c124c4dc3ddecdfd868a6
Ryan Workman <rdavid1099@gmail.com>
2019-05-06 16:54:48 -0700

    Refine countdown calculation to get current time and minutes left

--- app/controllers/concerns/date_helper.rb
+++ app/controllers/concerns/date_helper.rb
@@ -1,15 +1,16 @@
 module DateHelper
   def self.time_until_vacation
-    today = Date.today
-    trip_date = Date.new(2019, 10, 5)
-    total_days = (trip_date - today).to_i
+    seconds_left = Time.parse(Date.new(2019, 10, 5).to_s) - Time.now
+    minutes_left = seconds_left / 60
+    hours_left = minutes_left / 60
+    days_left = (hours_left / 24).to_i
     time = total_days/30 > 0 ? total_days/30 : total_days%30
     time_term = total_days/30 > 0 ? 'month' : 'day'
-    minutes = total_days * 1440
     {
       complete: "#{time == 1 ? 'is' : 'are'} about #{time} #{time_term.pluralize(time)}",
-      days: "#{total_days} #{'day'.pluralize(total_days)}",
-      minutes: "#{minutes} #{'minute'.pluralize(minutes)}"
+      days: "#{days_left} #{'day'.pluralize(days_left)}",
+      minutes: "#{minutes_left.to_i} #{'minute'.pluralize(minutes_left)}",
+      seconds: "#{seconds_left.to_i} #{'second'.pluralize(seconds_left)}"
     }
   end
 end
