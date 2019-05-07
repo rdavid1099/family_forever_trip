97844ae361c8764d25d93f2f2ccc72cbf8b99b84
Ryan Workman <rdavid1099@gmail.com>
2019-05-07 08:46:47 -0700

    Refactor routes and controller to be RESTful

--- config/routes/api.rb
+++ config/routes/api.rb
@@ -1,7 +1,7 @@
 namespace :api, defaults: { format: :json } do
   namespace :slack do
     namespace :slash do
-      post 'countdown', to: 'commands#countdown'
+      resources :countdown, only: [:create]
     end
   end
 end
