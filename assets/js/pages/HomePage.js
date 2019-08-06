e7e5298c3ac5413be2bb3dfbfd89a90c093fd788
rdavid1099 <rdavid1099@gmail.com>
2019-08-06 14:50:59 -0700

    Add styled-components

--- assets/js/pages/HomePage.js
+++ assets/js/pages/HomePage.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-export default function HomePage(props) {
+import {Wrapper} from '../styledComponents'
 
+export default function HomePage(props) {
   return (
-    <>
+    <Wrapper className="container">
       <div style={{textAlign: "center"}}>
         <h1>FamilyForeverTrip</h1>
         <p>A place for our family and friends to share their experiences on our first annual FamilyForeverTrip ❤️</p>
       </div>
 
       <img src="https://media.giphy.com/media/S5JSwmQYHOGMo/giphy.gif" alt="Under Construction" style={{margin: "125px 45% 0 45%"}} />
-    </>
+    </Wrapper>
   )
 }
