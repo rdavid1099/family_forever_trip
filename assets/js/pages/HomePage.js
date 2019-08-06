83b1e8b5b7f6b990cfb50ddbe152e2814e406c76
rdavid1099 <rdavid1099@gmail.com>
2019-08-06 13:20:50 -0700

    Add bootstrap and styled components

--- assets/js/pages/HomePage.js
+++ assets/js/pages/HomePage.js
@@ -10,7 +10,7 @@ export default function HomePage(props) {
         <p>A place for our family and friends to share their experiences on our first annual FamilyForeverTrip ❤️</p>
       </div>
 
-      <img src="https://media.giphy.com/media/S5JSwmQYHOGMo/giphy.gif" alt="Under Construction" style={{margin: "125px 45% 0 45%"}} />
+
     </>
   )
 }
